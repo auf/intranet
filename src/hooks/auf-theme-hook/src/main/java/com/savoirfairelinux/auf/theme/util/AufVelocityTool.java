53b58eb7abd1227878598b12a82522794f02ba91
Stefan Langenmaier <stefan.langenmaier@savoirfairelinux.com>
2013-09-23 10:47:34 -0400

    Tâche / Task #29905 : Créer un gabarit pour les sites et organisations the weather portlet now takes the information about the location from the address of the current user

--- src/hooks/auf-theme-hook/src/main/java/com/savoirfairelinux/auf/theme/util/AufVelocityTool.java
+++ src/hooks/auf-theme-hook/src/main/java/com/savoirfairelinux/auf/theme/util/AufVelocityTool.java
@@ -3,10 +3,15 @@ package com.savoirfairelinux.auf.theme.util;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.model.Address;
 import com.liferay.portal.model.Organization;
+import com.liferay.portal.model.User;
 import com.liferay.portal.service.OrganizationLocalServiceUtil;
+import com.liferay.portal.service.UserLocalServiceUtil;
 
 public class AufVelocityTool
 {
@@ -37,6 +42,38 @@ public class AufVelocityTool
 			log.error("Cannot get regions for organization " + organizationId);
 			return new ArrayList<Organization>();
 		}
+	}
+	
+	public String getUserWeatherLocation(long userId) {
+		User u = null;
+		try {
+			u = UserLocalServiceUtil.getUser(userId);
+		} catch (PortalException e1) {
+			//no user logged in or found
+		} catch (SystemException e1) {
+			//no user logged in or found
+		}
+		
+		if (u==null) {
+			return "Paris, France";
+		}
+		
+		try {
+			List<Address> addresses = u.getAddresses();
+			if (addresses.size() > 0) {
+				String city = addresses.get(0).getCity();
+				String country = addresses.get(0).getCountry().getName();
+				if (city!=null && city.length() > 0) {
+					if (country!=null && country.length() > 0) {
+						return city + ", " + country;
+					}
+					return city;
+				}
+			}
+		} catch (SystemException e) {
+			//no addresses were found
+		}
+		return "Paris, France";
 	}
 
 }
