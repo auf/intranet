63151259ba8e664f41aef38737e45e3202aa2fb1
bchhun <bernard.chhun@gmail.com>
2013-12-03 16:30:14 -0500

    #35307 adjustment in the region/pole/espace collabo index page

--- src/themes/auf-theme/src/main/webapp/js/main.js
+++ src/themes/auf-theme/src/main/webapp/js/main.js
@@ -2,9 +2,31 @@ var AUF = function(){
 	return {
 		init: function(){
 			this.portlet_activite_image_vide();
+			this.hide_employees();
 		},
 		portlet_activite_image_vide: function(){
 			$(".news .photo img[src='']").attr("src", "/auf-theme/images/logo.jpg");
+		},
+		hide_employees: function(){
+			var max_employees = 12,
+				employee_selector = ".portlet-boundary_aufuserlist_WAR_aufsync_ .box_centre .news";
+
+			if ( $(".portlet-boundary_aufuserlist_WAR_aufsync_").length > 0 ){
+				/* hide all other employees higher than max_employees */
+				$(employee_selector).slice(max_employees).hide();
+
+				/* add button to show 'em all after the 12th element */
+				$(employee_selector + ":eq(11)").after(
+					'<div class="clearfix"><input class="aui-button-input auf-show-all-employees" type="button" value="Afficher tous les employés"></div>'
+				);
+
+				$("input.auf-show-all-employees").click(function(){
+					$(employee_selector).slice(max_employees).fadeIn();
+					$(this).fadeOut(function(){
+						$(this).parent().remove()
+					});
+				});
+			}
 		}
 	}
 }();
