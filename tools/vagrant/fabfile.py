34f2b85217cd02336a30a4115ab9b9198aa2be6a
bchhun <bernard.chhun@gmail.com>
2014-01-14 09:54:49 -0500

    fabfile tweaks

--- tools/vagrant/fabfile.py
+++ tools/vagrant/fabfile.py
@@ -1,13 +1,16 @@
 #coding: utf-8
 
+from platform import system
 import os.path as op
 
-from fabric.api import env, task, sudo, run, cd, lcd, local
+from fabric.api import env, task, sudo, run, cd, lcd, local, put
 
 env.roledefs['vagrant'] = ["vagrant@10.0.0.17"]
 env.roles = ['vagrant']
 
 role = env.roles[0]
+
+THEME_NAME = "auf-theme"
 
 if role == 'vagrant':
     if not op.exists(op.expanduser('~/.ssh/config')):
@@ -16,23 +19,113 @@ if role == 'vagrant':
             fp.write("IdentityFile ~/.ssh/id_rsa")
     local('ssh-add %s' % op.expanduser('~/.vagrant.d/insecure_private_key'))
 
+@task
 def mount():
-    local("sudo mount -O soft,timeo=5,retrans=5,actimeo=10,retry=5 -o nolock 10.0.0.17:/opt/liferay-portal mount/")
+    """
+    [TODO] à faire pour un Mac
+    """
 
+    if system() == "Linux":
+        local("sudo mount -O soft,timeo=5,retrans=5,actimeo=10,retry=5 -o nolock 10.0.0.17:/opt/liferay-portal mount/")
+
+
+
+@task
 def unmount():
-    local("sudo umount ./mount")
+    """
+    [TODO] à faire pour un Mac
+    """
 
+    if system() == "Linux":
+        local("sudo umount -fl ./mount")
+
+@task
 def catalina_out():
+    """
+    tail-ing the catalina.out file from the vagrant folder
+    """
     run("tail -f /opt/liferay-portal/tomcat/logs/catalina.out")
 
+@task
 def mvn_deploy_all():
+    """
+    Deploying every hooks and theme in the vagrant folder
+    """
     with lcd("../../src"):
-        local("mvn liferay:deploy")
+        local("mvn clean package liferay:deploy")
 
+@task
 def mvn_deploy_theme():
-    with lcd("../../src/themes/auf-theme"):
-        local("mvn liferay:deploy")
+    """
+    Deploying the current theme in the vagrant folder
+    """
+    with lcd("../../src/themes/%s" % THEME_NAME):
+        local("mvn clean package liferay:deploy")
 
+@task
+def compass_compile():
+    """
+    Running compass compile from the vagrant folder
+    """
+    with lcd("../../src/themes/auf-theme/src/main/webapp"):
+        local("compass compile")
+
+@task
+def compass_deploy():
+    compass_compile()
+    mvn_deploy_theme()
+
+@task
+def copy_assets(folder=None):
+    """
+    Copying all CSS and JS files in the VM
+    """
+    compass_compile()
+
+    if folder:
+        put(
+            "../../src/themes/auf-theme/src/main/webapp/%s" % folder,
+            "/opt/liferay-portal/tomcat/webapps/auf-theme/"
+        )
+    else:
+
+        assets_folders = [
+            "css",
+            "js",
+            # "images"
+        ]
+
+        for f in assets_folders:
+            put(
+                "../../src/themes/auf-theme/src/main/webapp/%s" % f,
+                "/opt/liferay-portal/tomcat/webapps/auf-theme/"
+            )
+
+@task
+def copy_deployables():
+    """
+    So we may copy any packages that aren't managed within maven in the VM
+
+    ex: license file, translation hooks.
+    """
+    file_types = [
+        "lpkg",
+        "xml",
+        "war"
+    ]
+
+    for ft in file_types:
+        put("./deployables/*.%s" % ft, "/opt/liferay-portal/deploy")
+
+@task
+def lfr_start():
+    sudo("/etc/init.d/liferay start")
+
+@task
+def lfr_stop():
+    sudo("/etc/init.d/liferay stop")
+
+@task
 def lfr_restart():
-    run("/etc/init.d/liferay stop")
-    run("/etc/init.d/liferay start")
+    lfr_stop()
+    lfr_start()
