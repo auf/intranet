0bd0b34917f7e4cef5c3eddb8015a07c87370dd8
bchhun <bernard.chhun@gmail.com>
2014-01-18 04:59:02 -0500

    fabfile and readme enhancement

--- src/themes/auf-theme/src/main/webapp/config.rb
+++ src/themes/auf-theme/src/main/webapp/config.rb
@@ -8,20 +8,5 @@ images_dir = "images"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-output_style = :expanded    # :compressed
+output_style = :compressed    # :compressed
 # line_comments = false
-
-TARGET_PATH="../../../../../../tools/vagrant/mount/tomcat-7.0.27/webapps/auf-theme/"
-ACTIVE=true
-watch "./**/*" do |project_dir, relative_path|
-  if File.exists?(File.join(project_dir, relative_path))
-    f = File.expand_path(File.join(project_dir, TARGET_PATH, relative_path))
-    dir = File.dirname(f)
-    unless File.exists?(dir) and File.directory?(dir)
-      FileUtils.mkdir_p dir, :noop => !ACTIVE, :verbose => true
-    end
-    FileUtils.cp File.join(project_dir, relative_path), f, :noop => !ACTIVE, :verbose => true
-  else
-    FileUtils.rm f, :force => true, :noop => !ACTIVE, :verbose => true
-  end
-end
