2e5c4669db0cd1b7267ac765e8f561a33a5c3135
caroca <camilo.aroca-cruzat@savoirfairelinux.com>
2014-05-28 10:26:23 -0400

    #48137 navigation dynamique

--- src/hooks/auf-theme-hook/src/main/java/com/savoirfairelinux/auf/theme/events/AufPreAction.java
+++ src/hooks/auf-theme-hook/src/main/java/com/savoirfairelinux/auf/theme/events/AufPreAction.java
@@ -1,26 +1,25 @@
 package com.savoirfairelinux.auf.theme.events;
 
+import com.liferay.portal.kernel.events.ActionException;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.savoirfairelinux.auf.theme.util.AufVelocityTool;
+
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.liferay.portal.kernel.events.ActionException;
-import com.liferay.portal.kernel.util.WebKeys;
-import com.savoirfairelinux.auf.theme.util.AufVelocityTool;
-
-public class AufPreAction extends com.liferay.portal.kernel.events.Action
-{
+public class AufPreAction extends com.liferay.portal.kernel.events.Action {
 	@Override
-	public void run(HttpServletRequest request, HttpServletResponse response) throws ActionException
-	{	
+	public void run(HttpServletRequest request, HttpServletResponse response) throws ActionException {
 		// theme
 		@SuppressWarnings("unchecked")
 		Map<String, Object> vmVariables = (Map<String, Object>) request.getAttribute(WebKeys.VM_VARIABLES);
-		if (vmVariables == null) vmVariables = new HashMap<String, Object>();
-		vmVariables.put("aufTool", AufVelocityTool.getInstance());
+		if (vmVariables == null)
+			vmVariables = new HashMap<String, Object>();
+		vmVariables.put("aufTool", new AufVelocityTool(request));
 		request.setAttribute(WebKeys.VM_VARIABLES, vmVariables);
-		
+
 	}
 }
--- src/hooks/auf-theme-hook/src/main/java/com/savoirfairelinux/auf/theme/util/AufVelocityTool.java
+++ src/hooks/auf-theme-hook/src/main/java/com/savoirfairelinux/auf/theme/util/AufVelocityTool.java
@@ -1,13 +1,12 @@
 package com.savoirfairelinux.auf.theme.util;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.liferay.portal.NoSuchLayoutException;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.kernel.velocity.VelocityEngineUtil;
 import com.liferay.portal.model.Address;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.model.Layout;
@@ -18,48 +17,50 @@ import com.liferay.portal.service.LayoutLocalServiceUtil;
 import com.liferay.portal.service.OrganizationLocalServiceUtil;
 import com.liferay.portal.service.PortletPreferencesLocalServiceUtil;
 import com.liferay.portal.service.UserLocalServiceUtil;
+import com.liferay.portal.theme.NavItem;
+import com.liferay.portal.theme.RequestVars;
+import com.liferay.portal.theme.ThemeDisplay;
 
-public class AufVelocityTool
-{
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class AufVelocityTool {
 	private static Log log = LogFactoryUtil.getLog(AufVelocityTool.class);
-	private static AufVelocityTool instance;
+	private final HttpServletRequest request;
 
-	private AufVelocityTool() {
+	public AufVelocityTool(HttpServletRequest request) {
+		this.request = request;
 	}
 
-	public static AufVelocityTool getInstance()
-	{
-		if (instance == null)
-			instance = new AufVelocityTool();
-		return instance;
-	}
-	
+	public List<NavItem> getNavigationItems() throws SystemException {
+		final List<Layout> layouts = LayoutLocalServiceUtil.getLayouts(10180, false, 0);
 
-	public List<Organization> getSubOrganizations(long organizationId)
-	{
-		try
-		{
+		final RequestVars requestVars = new RequestVars(request, (ThemeDisplay) request.getAttribute(WebKeys.THEME_DISPLAY), 0, 0, VelocityEngineUtil.getRestrictedToolsContext());
+
+		return NavItem.fromLayouts(requestVars, layouts);
+	}
+
+	public List<Organization> getSubOrganizations(long organizationId) {
+		try {
 			Organization organization = OrganizationLocalServiceUtil.getOrganization(organizationId);
 			return organization.getSuborganizations();
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			log.error("Cannot get regions for organization " + organizationId);
 			return new ArrayList<Organization>();
 		}
 	}
-	
-	public List<Group> getCollaborationSites(long userId)
-	{
-		try
-		{
+
+	public List<Group> getCollaborationSites(long userId) {
+		try {
 			User liferayUser = UserLocalServiceUtil.getUser(userId);
-			
+
 			List<Group> userSites = new ArrayList<Group>();
 			for (Group site : liferayUser.getGroups()) {
-			  if (site.isRegularSite()) {
-				  userSites.add(site);
-			  }
+				if (site.isRegularSite()) {
+					userSites.add(site);
+				}
 			}
 			return userSites;
 		} catch (PortalException e) {
@@ -72,13 +73,12 @@ public class AufVelocityTool
 			return new ArrayList<Group>();
 		}
 	}
-	
+
 	public String respondTest(long randomId) {
 		return "Test";
 	}
-	
-	public boolean isNotUnder(long organisationId, long plid)
-	{
+
+	public boolean isNotUnder(long organisationId, long plid) {
 		try {
 			Layout l = LayoutLocalServiceUtil.getLayout(plid);
 			if (l.getGroup().isGuest()) {
@@ -88,7 +88,7 @@ public class AufVelocityTool
 				return true;
 			}
 			Organization currentOrg = OrganizationLocalServiceUtil.getOrganization(l.getGroup().getOrganizationId());
-			
+
 			Organization possibleParentOrg = OrganizationLocalServiceUtil.getOrganization(organisationId);
 			for (Organization ancestor : currentOrg.getAncestors()) {
 				if (ancestor.equals(possibleParentOrg)) {
@@ -104,12 +104,11 @@ public class AufVelocityTool
 		}
 		return true;
 	}
-	
-	public boolean isNotUnderFrontpage(long plid)
-	{
+
+	public boolean isNotUnderFrontpage(long plid) {
 		try {
 			Layout l = LayoutLocalServiceUtil.getLayout(plid);
-			if ((l.getGroup().isGuest()) && (l.getGroup().getDefaultPublicPlid() == plid) || (l.getGroup().getOrganizationId() == 10805 || l.getGroup().getOrganizationId() == 10833 || l.getGroup().getOrganizationId() == 10901 )) {
+			if ((l.getGroup().isGuest()) && (l.getGroup().getDefaultPublicPlid() == plid) || (l.getGroup().getOrganizationId() == 10805 || l.getGroup().getOrganizationId() == 10833 || l.getGroup().getOrganizationId() == 10901)) {
 				return false;
 			}
 		} catch (PortalException e) {
@@ -121,12 +120,11 @@ public class AufVelocityTool
 		}
 		return true;
 	}
-	
-	public boolean isNotUnderFrontpageSite(long plid)
-	{
+
+	public boolean isNotUnderFrontpageSite(long plid) {
 		try {
 			Layout l = LayoutLocalServiceUtil.getLayout(plid);
-			if (l.getGroup().isGuest()  || (l.getGroup().getOrganizationId() == 10805 || l.getGroup().getOrganizationId() == 10833 || l.getGroup().getOrganizationId() == 10901 )) {
+			if (l.getGroup().isGuest() || (l.getGroup().getOrganizationId() == 10805 || l.getGroup().getOrganizationId() == 10833 || l.getGroup().getOrganizationId() == 10901)) {
 				return false;
 			}
 		} catch (PortalException e) {
@@ -138,42 +136,42 @@ public class AufVelocityTool
 		}
 		return true;
 	}
-	
+
 	public String getUserWeatherLocation(long userId) {
 		User u = null;
 		try {
 			u = UserLocalServiceUtil.getUser(userId);
 		} catch (PortalException e1) {
-			//no user logged in or found
+			// no user logged in or found
 		} catch (SystemException e1) {
-			//no user logged in or found
+			// no user logged in or found
 		}
-		
-		if (u==null) {
+
+		if (u == null) {
 			return "Paris, France";
 		}
-		
+
 		try {
 			List<Address> addresses = u.getAddresses();
 			if (addresses.size() > 0) {
 				String city = addresses.get(0).getCity();
 				String country = addresses.get(0).getCountry().getName();
-				if (city!=null && city.length() > 0) {
-					if (country!=null && country.length() > 0) {
+				if (city != null && city.length() > 0) {
+					if (country != null && country.length() > 0) {
 						return city + ", " + country;
 					}
 					return city;
 				}
 			}
 		} catch (SystemException e) {
-			//no addresses were found
+			// no addresses were found
 		}
 		return "Paris, France";
 	}
-	
+
 	public String getEventSetting(long groupId) {
 		// template asset publisher configuration
-		final String AUF_EVENT_TEMPLATE_PAGE = "/auf-event-template-page"; 
+		final String AUF_EVENT_TEMPLATE_PAGE = "/auf-event-template-page";
 		Layout l = null;
 		try {
 			l = LayoutLocalServiceUtil.getFriendlyURLLayout(groupId, false, AUF_EVENT_TEMPLATE_PAGE);
@@ -183,16 +181,15 @@ public class AufVelocityTool
 			log.error(AUF_EVENT_TEMPLATE_PAGE + " not found! Was looking in group " + groupId);
 			e.printStackTrace();
 		}
-		
-		if (l == null) return "";
-		
+
+		if (l == null)
+			return "";
+
 		try {
 			long plid = l.getPlid();
 			List<PortletPreferences> portlets = PortletPreferencesLocalServiceUtil.getPortletPreferences(0, 3, plid);
 			for (PortletPreferences p : portlets) {
-				if (p.getPortletId().startsWith("101") 
-						&& (!p.getPortletId().equals("101_INSTANCE_VORUZ3haQRVY")) 
-						&& (!p.getPortletId().equals("101_INSTANCE_eFzcOQLPj6zh"))) {
+				if (p.getPortletId().startsWith("101") && (!p.getPortletId().equals("101_INSTANCE_VORUZ3haQRVY")) && (!p.getPortletId().equals("101_INSTANCE_eFzcOQLPj6zh"))) {
 					return p.getPreferences();
 				}
 			}
@@ -202,10 +199,10 @@ public class AufVelocityTool
 		}
 		return "";
 	}
-	
+
 	public String getActualitySetting(long groupId) {
 		// template asset publisher configuration
-		final String AUF_ACTUALITY_TEMPLATE_PAGE = "/auf-actuality-template-page"; 
+		final String AUF_ACTUALITY_TEMPLATE_PAGE = "/auf-actuality-template-page";
 		Layout l = null;
 		try {
 			l = LayoutLocalServiceUtil.getFriendlyURLLayout(groupId, false, AUF_ACTUALITY_TEMPLATE_PAGE);
@@ -215,16 +212,15 @@ public class AufVelocityTool
 			log.error(AUF_ACTUALITY_TEMPLATE_PAGE + " not found! Was looking in group " + groupId);
 			e.printStackTrace();
 		}
-		
-		if (l == null) return "";
-		
+
+		if (l == null)
+			return "";
+
 		try {
 			long plid = l.getPlid();
 			List<PortletPreferences> portlets = PortletPreferencesLocalServiceUtil.getPortletPreferences(0, 3, plid);
 			for (PortletPreferences p : portlets) {
-				if (p.getPortletId().startsWith("101") 
-						&& (!p.getPortletId().equals("101_INSTANCE_VORUZ3haQRVY")) 
-						&& (!p.getPortletId().equals("101_INSTANCE_eFzcOQLPj6zh"))) {
+				if (p.getPortletId().startsWith("101") && (!p.getPortletId().equals("101_INSTANCE_VORUZ3haQRVY")) && (!p.getPortletId().equals("101_INSTANCE_eFzcOQLPj6zh"))) {
 					return p.getPreferences();
 				}
 			}
@@ -234,10 +230,10 @@ public class AufVelocityTool
 		}
 		return "";
 	}
-	
+
 	public String getRssSetting(long groupId) {
 		// template asset publisher configuration
-		final String AUF_RSS_TEMPLATE_PAGE = "/auf-flux-rss"; 
+		final String AUF_RSS_TEMPLATE_PAGE = "/auf-flux-rss";
 		Layout l = null;
 		try {
 			l = LayoutLocalServiceUtil.getFriendlyURLLayout(groupId, false, AUF_RSS_TEMPLATE_PAGE);
@@ -247,9 +243,10 @@ public class AufVelocityTool
 			log.error(AUF_RSS_TEMPLATE_PAGE + " not found! Was looking in group " + groupId);
 			e.printStackTrace();
 		}
-		
-		if (l == null) return "";
-		
+
+		if (l == null)
+			return "";
+
 		try {
 			long plid = l.getPlid();
 			List<PortletPreferences> portlets = PortletPreferencesLocalServiceUtil.getPortletPreferences(0, 3, plid);
